1710ee96823e2cbe311bbfd62f9376193c19caf9
NIkifor <ace.4four@list.ru>
2025-09-12 17:58:34 +0300

    commit

--- patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
+++ patient-service/src/main/java/com/pm/patientservice/grpc/BillingServiceGrpcClient.java
@@ -17,8 +17,7 @@ public class BillingServiceGrpcClient {
 
     public BillingServiceGrpcClient(
             @Value("${billing.service.address:localhost}") String serverAddress,
-            @Value("${billing.service.grpc.port:9001}") int serverPort
-    ){
+            @Value("${billing.service.grpc.port:9001}") int serverPort){
         log.info("Connecting to Billing Grpc Service at {}:{}" ,serverAddress, serverPort);
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress(serverAddress,
